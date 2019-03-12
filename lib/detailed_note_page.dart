49a00f805c85aec17b3f9bae65895722564cb359
sangeetagupta <tinnigupta1998@gmail.com>
2019-03-12 23:57:55 +0530

    7 commit

--- lib/detailed_note_page.dart
+++ lib/detailed_note_page.dart
@@ -12,16 +12,48 @@ class DetailedNotePage extends StatefulWidget{
 class _DetailedNotePageState extends State<DetailedNotePage> {
   @override
   Widget build(BuildContext context) {
+
+    DateTime dateTime  = DateTime.parse(widget.note.date);
+
     return Scaffold(
       appBar: AppBar(elevation: 0.0,
         backgroundColor: Colors.lightBlueAccent,),
-      body: Container( decoration: BoxDecoration(
+      body: Container(
+
+        padding: EdgeInsets.only(left: 20.0, right: 20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Text(widget.note.title,style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.w600,),),
+                  Text(dateTime.day.toString() + "/" + dateTime.month.toString() + "/" + dateTime.year.toString()),
+          ]
+      ),
+            Divider(),
+            TextField(
+              decoration: new InputDecoration.collapsed(
+                  hintText: widget.note.description,
+                  hintStyle: TextStyle(fontSize: 17.0,color: Colors.black87),
+              ),)
+          ],
+        ),
+          decoration: BoxDecoration(
         gradient: LinearGradient(
           begin: Alignment.topCenter,
           end: Alignment.bottomCenter,
           colors: [Colors.lightBlueAccent, Colors.deepPurpleAccent],
         ),
       )),
+      floatingActionButton : FloatingActionButton(
+          onPressed: (){},
+          backgroundColor: Colors.black,
+          child: Icon(Icons.mode_edit, color: Colors.white,),
+
+          ),
     );
   }
 }
